a5d4a2b819ac2b73f0f0461393f95d2609ddd1ec
QiuMing0011 <158472490+QiuMing0011@users.noreply.github.com>
2024-11-17 19:23:49 +0800

    Prescription.java

--- hms-app/src/main/java/hms/storage/Prescription.java
+++ hms-app/src/main/java/hms/storage/Prescription.java
@@ -1,38 +1,35 @@
+package storage;
 
+import enums.UserRole;
+import interfaces.IDataService;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import model.user.User;
 
 public class Prescription {
-    // Private member variables
     private int prescriptionID;
     private String prescriptionStatus;
-
-    // Constructor
     public Prescription(int prescriptionID, String prescriptionStatus) {
         this.prescriptionID = prescriptionID;
         this.prescriptionStatus = prescriptionStatus;
     }
-
-    // Getter for prescriptionID
     public int getPrescriptionID() {
         return prescriptionID;
     }
-
-    // Setter for prescriptionID
     public void setPrescriptionID(int prescriptionID) {
         this.prescriptionID = prescriptionID;
     }
-
-    // Getter for prescriptionStatus
     public String getPrescriptionStatus() {
         return prescriptionStatus;
     }
-
-    // Setter for prescriptionStatus
     public void setPrescriptionStatus(String prescriptionStatus) {
         this.prescriptionStatus = prescriptionStatus;
     }
-
-    // Method to display prescription details
-    public void displayPrescriptionDetails() {
+    public void updatePrescriptionStatus() {
         System.out.println("Prescription ID: " + prescriptionID);
         System.out.println("Prescription Status: " + prescriptionStatus);
     }
